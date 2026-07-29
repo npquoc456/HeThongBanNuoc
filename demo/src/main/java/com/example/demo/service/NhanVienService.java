ebe7cd90958db592f820110aaaf0718ba9fa612d
minh <n.p.quoc456@gmail.com>
2026-07-30 03:53:51 +0700

    update:

--- demo/src/main/java/com/example/demo/service/NhanVienService.java
+++ demo/src/main/java/com/example/demo/service/NhanVienService.java
@@ -11,8 +11,7 @@ import com.example.demo.repository.NhanVienRepository;
 
 @Service
 public class NhanVienService {
-    @Autowired 
-    private NhanVienRepository nhanVienRepository;
+    @Autowired private NhanVienRepository nhanVienRepository;
     
     public NhanVien DangNhap(Integer id, String matkhau){
         Optional<NhanVien> nhanVien = nhanVienRepository.TimNhanVienTheoIDvaMK(id, matkhau);
